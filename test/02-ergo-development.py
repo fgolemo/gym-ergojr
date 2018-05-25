5cdc7320be00e411073d5d9b55490ca6feef9637
Florian Golemo <fgolemo@gmail.com>
2018-05-25 22:37:16 +0200

    4 joints working

--- test/02-ergo-development.py
+++ test/02-ergo-development.py
@@ -11,13 +11,12 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
 p.resetDebugVisualizerCamera(cameraDistance=0.3,
                              cameraYaw=135,
                              cameraPitch=-45,
-                             cameraTargetPosition=[0,0,0])
+                             cameraTargetPosition=[0, 0, 0])
 
 p.setGravity(0, 0, -10)
-frequency = 100 # Hz
-p.setTimeStep(1/frequency)
+frequency = 100  # Hz
+p.setTimeStep(1 / frequency)
 p.setRealTimeSimulation(0)
-
 
 planeId = p.loadURDF("plane.urdf")
 
@@ -32,29 +31,31 @@ for i in range(p.getNumJoints(robot)):
     print(p.getJointInfo(robot, i))
 
 # leftWheels = [6,7]
-motors = [2,3]
+motors = [2, 3, 5, 7]
 
-motor1 = p.addUserDebugParameter("motor1",-1,1,0)
-motor2 = p.addUserDebugParameter("motor2",-1,1,0)
+debugParams = []
+
+for i in range(4):
+    motor = p.addUserDebugParameter("motor{}".format(i + 1), -1, 1, 0)
+    debugParams.append(motor)
+
 # forceSlider = p.addUserDebugParameter("maxForce",-10,10,0)
 
 start = time.time()
 
-for i in range(frequency*10):
-    motor1pos = (math.pi/2) * p.readUserDebugParameter(motor1)
-    motor2pos = (math.pi/2) * p.readUserDebugParameter(motor2)
+for i in range(frequency * 30):
+    motorPos = []
+    for i in range(4):
+        pos = (math.pi / 2) * p.readUserDebugParameter(debugParams[i])
+        motorPos.append(pos)
+        p.setJointMotorControl2(robot, motors[i], p.POSITION_CONTROL, targetPosition=pos)
+
     # maxForce = p.readUserDebugParameter(forceSlider)
 
-    # for wheel in leftWheels:
-    #     p.setJointMotorControl2(robot, wheel, p.VELOCITY_CONTROL, targetVelocity=leftVelocity, force=maxForce)
-    #
-    p.setJointMotorControl2(robot, motors[0], p.POSITION_CONTROL, targetPosition=motor1pos)
-    p.setJointMotorControl2(robot, motors[1], p.POSITION_CONTROL, targetPosition=motor2pos)
-
     p.stepSimulation()
-    time.sleep(1. / frequency )
+    time.sleep(1. / frequency)
 
-print (time.time() - start)
+print(time.time() - start)
 
 pos, orn = p.getBasePositionAndOrientation(robot)
 print(pos, orn)
