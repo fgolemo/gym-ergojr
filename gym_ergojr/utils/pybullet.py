c613f58dd47fc65feb4b766a241e81f9f1b8a989
Florian Golemo <fgolemo@gmail.com>
2018-08-20 23:29:32 -0400

    more debugging stuff

--- gym_ergojr/utils/pybullet.py
+++ gym_ergojr/utils/pybullet.py
@@ -1,27 +1,29 @@
 import pybullet as p
 import numpy as np
 
+
 class DistanceBetweenObjects(object):
     def __init__(self, bodyA, bodyB, linkA, linkB):
         self.bodyA = bodyA
         self.bodyB = bodyB
         self.linkA = linkA
         self.linkB = linkB
-        self.goal = (0,0,0)
+        self.goal = (0, 0, 0)
 
     def query(self):
-        posA = p.getLinkState(bodyUniqueId=self.bodyA, linkIndex=self.linkA)
-        _ = [float(x) for x in posA[0]]
-        # rt = p.rayTest(self.goal,posA[1])
-        # dist = np.linalg.norm(np.array(self.goal)-np.array(rt[3]))
-        dist = np.linalg.norm(np.array(self.goal)-np.array(posA[0]))
-        if np.isnan(dist):
-            print ("pos:",posA)
-            print ("goal:",self.goal)
-            raise Exception("NaN dist")
+        for tries in range(5):
+            posA = p.getLinkState(bodyUniqueId=self.bodyA, linkIndex=self.linkA)
+            # rt = p.rayTest(self.goal,posA[1])
+            # dist = np.linalg.norm(np.array(self.goal)-np.array(rt[3]))
+            dist = np.linalg.norm(np.array(self.goal) - np.array(posA[0]))
 
-        return dist
-
+            if np.isnan(dist):
+                print(tries, "pos:", posA)
+                print(tries, "goal:", self.goal)
+                p.stepSimulation() # in case the sim just has a hiccup
+                continue
+            else:
+                return dist
 
         # contacts = p.getClosestPoints(bodyA=self.bodyA, bodyB=self.bodyB,
         #                               linkIndexA=self.linkA, linkIndexB=self.linkB,
@@ -32,6 +34,6 @@ class DistanceBetweenObjects(object):
 
 
 def sanitizeAction(action, alen):
-    action = np.clip(action,-1,1)
+    action = np.clip(action, -1, 1)
     assert len(action) == alen
     return action
