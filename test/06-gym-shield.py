394a3ae98ee730fcd9f5bee0c59e2d4175966bb1
Florian Golemo <fgolemo@gmail.com>
2019-03-25 16:30:28 -0400

    added comments

--- test/06-gym-shield.py
+++ test/06-gym-shield.py
@@ -3,16 +3,23 @@ import gym_ergojr
 import time
 import numpy as np
 
-env = gym.make("ErgoFightStatic-Graphical-Shield-Move-HalfRand-Bullet-v0")
+env = gym.make("ErgoFightStatic-Headless-Shield-Move-HalfRand-Bullet-v0")
 env.reset()
 
-for i in range(1005):
+start = time.time()
+
+for i in range(10000):
     if i % 50 == 0:
         action = env.action_space.sample()
     obs, rew, done, _ = env.step(action)
+    if done:
+        env.reset()
     # if rew == 1:
     #     print(i, np.around(obs, 1), rew)
 
-    print(i, rew, done)
+    # print(i, rew, done)
 
     # time.sleep(0.01)
+
+end = time.time() - start
+print ("total time: ",end,". FPS: ",10000/end)
---
+++ test/17-jackal-development.py
@@ -0,0 +1,65 @@
+import math
+import pybullet as p
+import time
+import pybullet_data
+import numpy as np
+
+from gym_ergojr import get_scene
+from gym_ergojr.utils.urdf_helper import URDF
+
+physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
+p.resetDebugVisualizerCamera(cameraDistance=0.45,
+                             cameraYaw=135,
+                             cameraPitch=-45,
+                             cameraTargetPosition=[0, 0, 0])
+
+p.setGravity(0, 0, -10)
+frequency = 100  # Hz
+p.setTimeStep(1 / frequency)
+p.setRealTimeSimulation(0)
+
+planeId = p.loadURDF("plane.urdf")
+
+startPos = [0, 0, .1]  # RGB = xyz
+startOrientation = p.getQuaternionFromEuler([0, 0, 0])  # rotated around which axis? # np.deg2rad(90)
+# rotating a standing cylinder around the y axis, puts it flat onto the x axis
+
+xml_path = get_scene("jackal")
+
+robot_file = URDF(xml_path, force_recompile=True).get_path()
+robot = p.loadURDF(robot_file, startPos, startOrientation, useFixedBase=0)
+
+for i in range(p.getNumJoints(robot)):
+    print(p.getJointInfo(robot, i))
+
+# leftWheels = [6,7]
+motors = [1,2,3,4,]
+
+debugParams = []
+
+for i in range(len(motors)):
+    motor = p.addUserDebugParameter("motor{}".format(i + 1), -1, 1, 0)
+    debugParams.append(motor)
+
+# forceSlider = p.addUserDebugParameter("maxForce",-10,10,0)
+
+start = time.time()
+
+for i in range(frequency * 30):
+    motorPos = []
+    for i in range(len(motors)):
+        pos = (math.pi / 2) * p.readUserDebugParameter(debugParams[i])
+        motorPos.append(pos)
+        p.setJointMotorControl2(robot, motors[i], p.POSITION_CONTROL, targetPosition=pos)
+
+    # maxForce = p.readUserDebugParameter(forceSlider)
+
+    p.stepSimulation()
+    time.sleep(1. / frequency)
+
+print(time.time() - start)
+
+pos, orn = p.getBasePositionAndOrientation(robot)
+print(pos, orn)
+p.disconnect()
