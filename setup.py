2a3079ecee83b7edda46434b6b93236ed0fcb28e
Florian Golemo <fgolemo@gmail.com>
2019-05-16 18:26:47 -0400

    version bump for pusher

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup
 
-setup(name='gym_ergojr',
-      version='1.2',
-      install_requires=['gym>=0.2.3',
-                        'pybullet>=1.9.4',
-                        'sklearn',
-                        'scipy',
-                        "tqdm",
-                        "matplotlib",
-                        "numpy"
-                        ]
-)
+setup(
+    name='gym_ergojr',
+    version='1.3',
+    install_requires=[
+        'gym>=0.2.3', 'pybullet>=1.9.4', 'sklearn', 'scipy', "tqdm",
+        "matplotlib", "numpy"
+    ])
